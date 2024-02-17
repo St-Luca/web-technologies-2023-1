ad30e4bc24052672754a5ae7b33be19c8accd3d6
a.lukyanchenko <a.lukyanchenko@smartway.today>
2024-02-17 23:56:07 +0500

    small save

--- src/test.php
+++ src/test.php
@@ -1,65 +1,45 @@
 <?php
+$php_ini_path = php_ini_loaded_file();
+echo $php_ini_path;
 
 //Задание 1
-$a = rand(-10, 10);
-$b = rand(-10, 10);
+function printNumbersWithMessages()
+{
+    $number = 0;
 
-if ($a >= 0 && $b >= 0) {
-    $result = $a - $b;
-    echo "Разность $a и $b равна $result";
-} elseif ($a < 0 && $b < 0) {
-    $result = $a * $b;
-    echo "Произведение $a и $b равно $result";
-} else {
-    $result = $a + $b;
-    echo "Сумма $a и $b равна $result";
+    do {
+        if ($number === 0) {
+            echo $number . " - это ноль. <br>";
+        } elseif ($number % 2 === 0) {
+            echo $number . " - чётное число.<br>";
+        } else {
+            echo $number . " - нечётное число.<br>";
+        }
+
+        $number++;
+    } while ($number <= 10);
 }
+
+echo printNumbersWithMessages();
 
 echo "<br>";
 echo "<br>";
 
 
 //Задание 2
-$a = rand(0, 15);
+$regions = array(
+    "Московская область" => array("Москва", "Зеленоград", "Клин"),
+    "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+    "Рязанская область" => array("Рязань", "Касимов", "Рыбное"),
+    "Самарская область" => array("Самара", "Тольятти", "Новокуйбышевск"),
+    "Тюменская область" => array("Тюмень", "Тобольск", "Ишим"),
+    "Краснодарский край" => array("Краснодар", "Сочи", "Анапа", "Ейск"),
+    "Свердловская область" => array("Екатеринбург", "Нижний Тагил", "Каменск-Уральский")
+);
 
-echo "Значение переменной \"a\": $a<br>";
-
-switch ($a) {
-    case 0:
-        echo "0<br>";
-    case 1:
-        echo "1<br>";
-    case 2:
-        echo "2<br>";
-    case 3:
-        echo "3<br>";
-    case 4:
-        echo "4<br>";
-    case 5:
-        echo "5<br>";
-    case 6:
-        echo "6<br>";
-    case 7:
-        echo "7<br>";
-    case 8:
-        echo "8<br>";
-    case 9:
-        echo "9<br>";
-    case 10:
-        echo "10<br>";
-    case 11:
-        echo "11<br>";
-    case 12:
-        echo "12<br>";
-    case 13:
-        echo "13<br>";
-    case 14:
-        echo "14<br>";
-    case 15:
-        echo "15<br>";
-        break;
-    default:
-        echo "Некорректное значение<br>";
+foreach ($regions as $region => $cities) {
+    echo $region . ":<br>";
+    echo implode(", ", $cities) . ".<br>";
 }
 
 echo "<br>";
@@ -67,104 +47,50 @@ echo "<br>";
 
 
 //Задание 3
-function addition($a, $b)
-{
-    return $a + $b;
-}
+$alphabet = array(
+    'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+    'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+    'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'ts',
+    'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+);
 
-function subtraction($a, $b)
+function transliterate($string, $alphabet)
 {
-    return $a - $b;
-}
+    $transliterated = '';
+    $string = mb_strtolower($string, 'UTF-8');
+    $length = mb_strlen($string, 'UTF-8');
 
-function multiplication($a, $b)
-{
-    return $a * $b;
-}
+    for ($i = 0; $i < $length; $i++) {
+        $char = mb_substr($string, $i, 1, 'UTF-8');
 
-function division($a, $b)
-{
-    if ($b == 0) {
-        return "Ошибка: деление на ноль";
-    } else {
-        return $a / $b;
+        if (isset($alphabet[$char])) {
+            $transliterated .= $alphabet[$char];
+        } else {
+            $transliterated .= $char;
+        }
     }
+
+    return $transliterated;
 }
 
-$a = 3;
-$b = 7;
-
-echo "Операции для чисел $a и $b: " . "<br>";
-echo "Сумма: " . addition($a, $b) . "<br>";
-echo "Разность: " . subtraction($a, $b) . "<br>";
-echo "Произведение: " . multiplication($a, $b) . "<br>";
-echo "Частное: " . division($a, $b) . "<br>";
+$string = "Транслитерация завершена успешно";
+echo transliterate($string, $alphabet);
 
 echo "<br>";
 echo "<br>";
 
 
 //Задание 4
-function mathOperation($arg1, $arg2, $operation)
-{
-    switch ($operation) {
-        case 'addition':
-            return addition($arg1, $arg2);
-            break;
-        case 'subtraction':
-            return subtraction($arg1, $arg2);
-            break;
-        case 'multiplication':
-            return multiplication($arg1, $arg2);
-            break;
-        case 'division':
-            return division($arg1, $arg2);
-            break;
-        default:
-            return "Ошибка: неверная операция";
-            break;
-    }
-}
 
-$a = 3;
-$b = 7;
-
-echo "Операции для чисел $a и $b: " . "<br>";
-echo "Сумма: " . mathOperation($a, $b, 'addition') . "<br>";
-echo "Разность: " . mathOperation($a, $b, 'subtraction') . "<br>";
-echo "Произведение: " . mathOperation($a, $b, 'multiplication') . "<br>";
-echo "Частное: " . mathOperation($a, $b, 'division') . "<br>";
-
-echo "<br>";
-echo "<br>";
-
-
-//Задание 5
-echo "Текущий год: " . date('Y') . "<br>";
-
-$dateInfo = getdate();
-echo "Текущий год: " . $dateInfo['year'] . "<br>";
-
-$date = new DateTime();
-echo "Текущий год: " . $date->format('Y') . "<br>";
-
-echo "<br>";
-echo "<br>";
 
 
 //Задание 6
-function power($val, $pow)
-{
-    if ($pow === 0) {
-        return 1;
+foreach ($regions as $region => $cities) {
+    echo $region . ":<br>";
+    foreach ($cities as $city) {
+        if (mb_substr($city, 0, 1, 'UTF-8') === 'К') {
+            echo $city . "<br>";
+        }
     }
-    if ($pow > 0) {
-        return $val * power($val, $pow - 1);
-    } else {
-        return 1 / ($val * power($val, abs($pow) - 1));
-    }
+    echo "<br>";
 }
-
-$val = 2;
-$pow = 10;
-echo "$val в степени $pow равно: " . power($val, $pow);
